741bc18bdf9bbe4130acaf5d3cea8e62ba35f917
Derick Anderson <derick@startitecture.com>
2018-03-25 21:32:05 -0400

    Removing all old SAF folders in preparation for sync to GitHub

--- SAF.ProcessEngine.Tests/FakeItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SAF.ProcessEngine.Tests
-{
-    public class FakeItem
-    {
-        public string StringValue { get; set; }
-
-        public int IntegerValue { get; set; }
-    }
-}
--- SAF.ProcessEngine/ExecuteCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SAF.ProcessEngine
-{
-    /// <summary>
-    /// Delegate for asynchronous command execution.
-    /// </summary>
-    /// <param name="parameter">Data used by the command. If the command does not require data to be passed, this 
-    /// object can be set to null.</param>
-    public delegate void ExecuteCommand(object parameter);
-}
